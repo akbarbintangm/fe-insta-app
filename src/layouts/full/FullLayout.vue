97617b3042ea5ad88d67721c3534aeaf9bddbf08
Akbar Bintang M <akbarbintangmahendra@gmail.com>
2025-11-06 00:11:57 +0700

    Add: Penambahan Modal secara global

--- src/layouts/full/FullLayout.vue
+++ src/layouts/full/FullLayout.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+  import { router } from '@/router';
+  import { ref, onMounted, watch } from 'vue'
   import { RouterView } from 'vue-router';
   import VerticalSidebarVue from './vertical-sidebar/VerticalSidebar.vue';
   import VerticalHeaderVue from './vertical-header/VerticalHeader.vue';
@@ -6,6 +8,45 @@
   import FooterPanel from './footer/FooterPanel.vue';
   import { useCustomizerStore } from '../../stores/customizer';
   const customizer = useCustomizerStore();
+
+  import ModalDialogMessages from '@/components/shared/ModalDialog.vue';
+  import { useModalStore } from '@/stores/components/modal';
+  const modal = useModalStore();
+
+  import ModalConfirmationMessages from '@/components/shared/ConfirmationDialog.vue';
+  import { useConfirmStore } from '@/stores/components/confirmation';
+  const confirm = useConfirmStore();
+
+  import IsUsernameIsset from '@/components/apps/IsUsernameIsset.vue';
+  const showUsernameDialog = ref(false);
+  import { useAuthStore } from '@/stores/auth';
+  const authStore = useAuthStore();
+
+  onMounted(() => {
+    if (!authStore.user?.userData?.username) {
+      showUsernameDialog.value = true
+    }
+  })
+
+  watch(
+    () => authStore.user?.userData,
+    (value) => {
+      if (!value) {
+        router.push('/login');
+      }
+    },
+    { immediate: true }
+  )
+
+  watch(
+    () => authStore.user?.userData?.username,
+    (value) => {
+      if (!value) {
+        showUsernameDialog.value = true
+      }
+    },
+    { immediate: true }
+  )
 </script>
 
 <template>
@@ -18,6 +59,9 @@
       <v-main>
         <v-container fluid class="page-wrapper">
           <div>
+            <ModalConfirmationMessages v-model="confirm.confirmDialogActive" :message="confirm.confirmDialogText" />
+            <ModalDialogMessages v-model="modal.messageDialogActive" :message="modal.messageDialogText" />
+            <IsUsernameIsset v-model="showUsernameDialog" />
             <RouterView />
             <!-- <v-btn class="customizer-btn" size="large" icon variant="flat" color="primary" @click.stop="customizer.SET_CUSTOMIZER_DRAWER(!customizer.Customizer_drawer)">
               <SettingsIcon class="icon" />
