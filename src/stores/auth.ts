3c23820fc3b0b1f59a861d309ac9bc60e825a226
Akbar Bintang M <akbarbintangmahendra@gmail.com>
2025-11-06 00:11:04 +0700

    Add: Auth Refresh

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -2,6 +2,9 @@ import { defineStore } from 'pinia';
 import { router } from '@/router';
 import { fetchWrapper } from '@/utils/helpers/fetch-wrapper';
 
+import { pageLoadingState } from '@/utils/helpers/page-loading';
+import NProgress from 'nprogress';
+
 const baseUrl = `${import.meta.env.VITE_API_URL}/api/auth`;
 
 export const useAuthStore = defineStore('auth', {
@@ -13,6 +16,8 @@ export const useAuthStore = defineStore('auth', {
 
   actions: {
     async login(email: string, password: string, remember: boolean = false) {
+      pageLoadingState.isLoading = true;
+      NProgress.start();
       try {
         const response = await fetchWrapper.post(`${baseUrl}/login`, {
           email: email,
@@ -30,10 +35,15 @@ export const useAuthStore = defineStore('auth', {
         router.push(this.returnUrl || '/');
       } catch (error: any) {
         throw error.message || error;
+      } finally {
+        pageLoadingState.isLoading = false;
+        NProgress.done();
       }
     },
 
     async register(firstName: string, lastName: string, email: string, password: string) {
+      pageLoadingState.isLoading = true;
+      NProgress.start();
       try {
         let fullName = firstName + ' ' + (lastName ? ' ' + lastName : '');
         const response = await fetchWrapper.post(`${baseUrl}/register`, {
@@ -47,13 +57,52 @@ export const useAuthStore = defineStore('auth', {
         await this.login(email, password, true);
       } catch (error: any) {
         throw error.message || error;
+      } finally {
+        pageLoadingState.isLoading = false;
+        NProgress.done();
       }
     },
 
-    logout() {
-      this.user = null;
-      localStorage.removeItem('user');
-      router.push('/login');
+    async logout() {
+      pageLoadingState.isLoading = true;
+      NProgress.start();
+      try {
+        // seharusnya disini confirmation dulu pakai confirmation dialog tadi
+        const response = await fetchWrapper.post(`${baseUrl}/logout`, {});
+        if (response.status !== 'success') {
+          throw new Error(response.data.error || response.message || 'Logout gagal');
+        }
+        this.user = null;
+        localStorage.removeItem('user');
+        router.push('/login');
+      } catch (error: any) {
+        throw error.message || error;
+      } finally {
+        pageLoadingState.isLoading = false;
+        NProgress.done();
+      }
+    },
+
+    async refresh() {
+      pageLoadingState.isLoading = true;
+      NProgress.start();
+      try {
+        const response = await fetchWrapper.post(`${baseUrl}/refresh`, {});
+        if (response.status !== 'success') {
+          throw new Error(response.message || 'Refresh gagal');
+        }
+        const token = response.data?.token;
+        const userData = response.data?.user;
+        if (!token) throw new Error('Token tidak ditemukan dalam response');
+        this.user = { userData, token };
+        localStorage.setItem('user', JSON.stringify(this.user));
+        return;
+      } catch (error: any) {
+        throw error.message || error;
+      } finally {
+        pageLoadingState.isLoading = false;
+        NProgress.done();
+      }
     }
   }
 });
